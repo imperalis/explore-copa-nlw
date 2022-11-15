b4ddfd17dbb29b02b2b95cb408409b4579e981e8
dore <promptoattano@gmail.com>
2022-11-15 04:37:12 -0300

    many changes and added dates!

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createCard(date, day, games){
 }
 
 document.querySelector('#cards').innerHTML =
-createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia'))+
+createCard('20/11', 'domingo', createGame('qatar', '13:00', 'ecuador'))+
 
 createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland') +
     createGame('portugal', '16:00', 'uruguay'))+
